858ceb1e6cab911a03ea18c9fed62d78183be47a
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2024-12-03 00:04:08 -0500

    Uploaded Assignment 5

--- demo_11_variable_selection/data_mining_demo.R
+++ demo_11_variable_selection/data_mining_demo.R
@@ -1,6 +1,6 @@
 ##################################################
 #
-# ECO 6416.0028 Applied Business Research Tools
+# QMB 6316.0081 R for Business Analytics
 #
 # Data Mining Demo
 # Data Mining over many Irrelevant Variables
@@ -8,19 +8,21 @@
 # and two highly correlated substitutes.
 #
 # Lealand Morin, Ph.D.
-# Assistant Professor
-# Department of Economics
+# Adjunct Professor
 # College of Business
 # University of Central Florida
 #
-# August 25, 2023
+# December 2, 2024
 #
 ##################################################
 #
-# ECO6416_Data_Mining_A5 gives an example of a simple form
+# data_mining_demo gives an example of a simple form
 #   of data mining with OLS regression using simulated data.
 #   It estimates a model that illustrates the effects of
 #   measurement error, correlated variables and irrelevant variables.
+#
+# Dependencies:
+#   sim_tools.R
 #
 ##################################################
 
@@ -42,7 +44,7 @@ rm(list=ls(all=TRUE))
 # 4. Copy the command from the Console in the bottom left pane.
 # 5. Paste the command below:
 
-setwd("C:/Users/le279259/OneDrive - University of Central Florida/Desktop/ECO6416_Demos")
+setwd("~/GitHub/QMB6316F24/demo_11_variable_selection")
 
 
 # Now, RStudio should know where your files are.
@@ -55,11 +57,11 @@ setwd("C:/Users/le279259/OneDrive - University of Central Florida/Desktop/ECO641
 
 
 # Read function for sampling data.
-source('ECO6416_tools.R')
-# This is the same as running the ECO6416_tools.R script first.
-# It assumes that the script is saved in the same working folder.
+source('../tools/sim_tools.R')
+# This is the same as running the sim_tools.R script first.
+# It assumes that the script is saved in a folder called 'tools'.
 
-# The file ECO6416_tools.R must be in the working directory.
+# The file sim_tools.R must be in a folder called 'tools'.
 # If you an error message, make sure that the file is
 # located in your working directory.
 # Also make sure that the name has not changed.
@@ -69,26 +71,28 @@ source('ECO6416_tools.R')
 # Setting the Parameters
 ##################################################
 
-# Dependent Variable: Automobile values
+# Dependent Variable: Property values (in Millions)
 
-beta_0          <-   50000     # Intercept
-beta_mileage    <- -  0.20     # Slope coefficient for mileage
-beta_accident   <- -  5000     # Slope coefficient for accident
-beta_damage     <- - 20000     # Slope coefficient for damage
+# Parameters:
+beta_0          <-   0.10    # Intercept
+beta_income     <-   5.00    # Slope ceofficient for income
+beta_cali       <-   0.25    # Slope coefficient for California
+beta_earthquake <- - 0.50    # Slope coefficient for earthquake (when active in model)
+# beta_earthquake <-   0.00    # Slope coefficient for earthquake (when removed from model)
 
-# Distribution of mileage.
-avg_mileage <- 50000
-sd_mileage  <- 10000
+# Distribution of incomes (also in millions).
+avg_income <- 0.1
+sd_income <- 0.01
 
-# Extra parameter for measurement error in mileage.
-number_of_mileage_variables <- 2
-measurement_error_mileage <- 5000
+# Extra parameter for measurement error in income.
+number_of_income_variables <- 2
+measurement_error_income <- 0.002
 
-# Fraction of dataset in an accident.
-pct_accident <- 0.4
+# Fraction of dataset in California.
+pct_in_cali <- 0.5
 
-# Frequency of damages (only after an accident).
-prob_damage <- 0.20 # More damage to illustrate the difference.
+# Frequency of earthquakes (only in California).
+prob_earthquake <- 0.075
 
 # Frequency of rainfall (can happen anywhere).
 prob_rainfall <- 0.25
@@ -97,7 +101,7 @@ prob_rainfall <- 0.25
 number_of_rainfall_variables <- 20
 
 # Additional terms:
-sigma_2 <- 4000    # Variance of error term
+sigma_2 <- 0.1        # Variance of error term
 num_obs <- 200        # Number of observations in entire dataset
 num_obs_estn <- 100   # Number of observations for estimation.
 # Notice num_obs is twice as large, saving half for out-of-sample testing.
@@ -114,32 +118,33 @@ table(obsns_for_estimation)
 # The relevant data in the model
 ##################################################
 
-# Call the other_sample function from ECO6416_tools_3.R.
-car_data <- other_sample(beta_0, beta_mileage, beta_accident, beta_damage,
-                               avg_mileage, sd_mileage, pct_accident, prob_damage,
+# Call the housing_sample function from ECO6416_tools_3.R.
+housing_data <- housing_sample(beta_0, beta_income, beta_cali, beta_earthquake,
+                               avg_income, sd_income, pct_in_cali, prob_earthquake,
                                sigma_2, num_obs,
-                               number_of_mileage_variables, measurement_error_mileage,
+                               number_of_income_variables, measurement_error_income,
                                number_of_rainfall_variables, prob_rainfall)
 
 # Summarize the data.
-summary(car_data)
+summary(housing_data)
 
-# Check that damages occurred only in accidents:
-table(car_data[, 'accident'], car_data[, 'damage'])
+# Check that earthquakes occurred only in California:
+table(housing_data[, 'in_cali'], housing_data[, 'earthquake'])
 # Data errors are the largest cause of problems in model-building.
 
-# Check the subsamples for estimation and testing.
+# Check for the subsamples for estimation and testing.
 # Estimation sample:
-table(car_data[obsns_for_estimation, 'accident'],
-      car_data[obsns_for_estimation, 'damage'])
+table(housing_data[obsns_for_estimation, 'in_cali'],
+      housing_data[obsns_for_estimation, 'earthquake'])
 # Testing sample:
-table(car_data[!obsns_for_estimation, 'accident'],
-      car_data[!obsns_for_estimation, 'damage'])
+table(housing_data[!obsns_for_estimation, 'in_cali'],
+      housing_data[!obsns_for_estimation, 'earthquake'])
 # ! means 'not'.
 # So, !obsns_for_estimation means to include only the
 # observations left out for testing the model.
 
-# Run the script again if there are not damages
+# Run the housing_data <- housing_sample(...)
+# block of code again if there are not earthquakes
 # in both samples.
 
 
@@ -149,24 +154,24 @@ table(car_data[!obsns_for_estimation, 'accident'],
 ##################################################
 
 #--------------------------------------------------
-# Assume that true mileage is not observed but some variables
-# that are correlated with mileage are available.
+# Assume that true income is not observed but some variables
+# that are correlated with income are available.
 #--------------------------------------------------
 
-mileage_variable_list <- sprintf('mileage_%d', seq(1:number_of_mileage_variables))
+income_variable_list <- sprintf('income_%d', seq(1:number_of_income_variables))
 # These variables are created in the ECO6416_tools_3.R script.
 
 
 # Check how strongly the data are correlated.
-cor(car_data[, c('mileage', 'mileage_1', 'mileage_2')])
+cor(housing_data[, c('income', 'income_1', 'income_2')])
 
-correl_mileage_1_2 <- cor(car_data[, 'mileage'],
-                         car_data[, 'mileage_1'])
-plot(car_data[, 'mileage'], car_data[, 'mileage_1'],
-     main = c('Scattergraph of two measures of mileage',
-              sprintf('(r = %f)', correl_mileage_1_2)),
-     xlab = 'mileage',
-     ylab = 'mileage 1')
+correl_income_1_2 <- cor(housing_data[, 'income'],
+                         housing_data[, 'income_1'])
+plot(housing_data[, 'income'], housing_data[, 'income_1'],
+     main = c('Scattergraph of two measures of income',
+              sprintf('(r = %f)', correl_income_1_2)),
+     xlab = 'Income',
+     ylab = 'Income 1')
 
 #--------------------------------------------------
 # Further, assume that many rainfall variables
@@ -178,14 +183,14 @@ rainfall_variable_list <- sprintf('rainfall_%d', seq(1:number_of_rainfall_variab
 # These variables are also created in the ECO6416_tools_3.R script.
 
 # Summarize the data.
-summary(car_data)
+summary(housing_data)
 # Should be many rainfall variables.
 
 
 # Collect all available variables into a single list.
-variable_list <- c(mileage_variable_list, 'accident', 'damage',
+variable_list <- c(income_variable_list, 'in_cali', 'earthquake',
                    rainfall_variable_list)
-# Note that true mileage is not in this list.
+# Note that true income is not in this list.
 # We are pretending that it is unobserved.
 
 
@@ -195,8 +200,8 @@ variable_list <- c(mileage_variable_list, 'accident', 'damage',
 ##################################################
 
 # Estimate a regression model.
-lm_true_model <- lm(data = car_data[obsns_for_estimation, ],
-                    formula = car_price ~ mileage + accident + damage)
+lm_true_model <- lm(data = housing_data[obsns_for_estimation, ],
+                    formula = house_price ~ income + in_cali + earthquake)
 
 # Output the results to screen.
 summary(lm_true_model)
@@ -204,12 +209,12 @@ summary(lm_true_model)
 
 ##################################################
 # Estimating the Feasible Regression Model
-# Model 2: Include only the available mileage variables.
+# Model 2: Include only the available income variables.
 ##################################################
 
 # Estimate a regression model.
-lm_feasible_model <- lm(data = car_data[obsns_for_estimation, ],
-                        formula = car_price ~ mileage_1 + mileage_2 + accident + damage)
+lm_feasible_model <- lm(data = housing_data[obsns_for_estimation, ],
+                        formula = house_price ~ income_1 + income_2 + in_cali + earthquake)
 
 # Output the results to screen.
 summary(lm_feasible_model)
@@ -255,20 +260,20 @@ for (best_model_num in 1:length(variable_list)) {
     test_variable_name <- remaining_variable_list[test_model]
 
     # Create a temporary variable list.
-    fmla_string <- sprintf('car_price ~  %s',
+    fmla_string <- sprintf('house_price ~  %s',
                            paste(cbind(best_variable_list, test_variable_name),
                                  sep = '', collapse = ' + '))
     fmla <- as.formula(fmla_string)
 
     # Estimate the model.
-    lm_test_model <- lm(data = car_data[obsns_for_estimation, ],
+    lm_test_model <- lm(data = housing_data[obsns_for_estimation, ],
                         formula = fmla_string)
 
     # Calculate R^2 both in sample and out of sample.
     test_R2 <- summary(lm_test_model)$adj.r.squared # Pulled from estimation output.
     # Out-of-sample value needs to be calculated directly.
-    actual_out <- car_data[!obsns_for_estimation, 'car_price']
-    predict_out <- predict(lm_test_model, newdata = car_data[!obsns_for_estimation, ])
+    actual_out <- housing_data[!obsns_for_estimation, 'house_price']
+    predict_out <- predict(lm_test_model, newdata = housing_data[!obsns_for_estimation, ])
     test_R2_out <- 1 - ( sum((actual_out - predict_out )^2) /
                            (num_obs_estn - best_model_num - 1) ) /
       ( sum((actual_out - mean(actual_out))^2) /
@@ -297,7 +302,7 @@ for (best_model_num in 1:length(variable_list)) {
 
   # Print a progress report for this model.
   print(sprintf('The best model with %d variables is ', best_model_num))
-  fmla_string <- sprintf('car_price ~  %s',
+  fmla_string <- sprintf('house_price ~  %s',
                          paste(best_variable_list, sep = '', collapse = ' + '))
   print(fmla_string)
   print(sprintf('with an R-squared of %f.', best_R2_so_far))
